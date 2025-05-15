8c9473d2ec1b5223d427c76d9f0a152d357d778b
777Luis777 <147398064+777Luis777@users.noreply.github.com>
2025-05-15 19:10:17 +0100

    Add files via upload

---
+++ ProjetoGulp/dist/queries_combinadas.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE 2p_gulp;
+
+USE 2p_gulp;
+CREATE TABLE alunos (
+
+    alunoid int AUTO_INCREMENT,
+
+    nome varchar(255),
+
+    numeroaluno int DEFAULT 0,
+
+    email varchar(255),
+
+
+    PRIMARY KEY(alunoid)
+
+);
+INSERT INTO alunos (nome, numeroaluno, email) VALUES ('João Carvalho', 1001, 'joao.carvalho@oficina.pt');
+
+INSERT INTO alunos (nome, numeroaluno, email) VALUES ('Maria Silva', 1002, 'maria.silva@oficina.pt');
+
+INSERT INTO alunos (nome, numeroaluno, email) VALUES ('Pedro Almeida', 1003, 'pedro.almeida@oficina.pt');
